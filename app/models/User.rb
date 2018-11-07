68f2b2fbdfd114245f926d8af6a21473e04efa8a
cfernando415 <ca.fernando415@gmail.com>
2018-11-07 18:58:50 -0500

    working on safe_recipes method on the user class

--- app/models/User.rb
+++ app/models/User.rb
@@ -12,7 +12,7 @@ class User
 #establishes one to many relationship with RecipeCard class
 #creates an indirect relationship with Recipe class through the RecipeCard class
   def recipes
-    RecipeCard.all.select {|recipecard| recipecard.user == self}
+    RecipeCard.all.select {|recipecard| recipecard.user == self}.map{|recipecard| recipecard.recipe}
   end
   def add_recipe_card(recipe, date, rating)
     RecipeCard.new(date, rating, self, recipe)
@@ -21,12 +21,24 @@ class User
     Allergen.new(self, ingredient)
   end
   def allergens
-    Allergen.all.select {|allergen| allergen.user == self}
+    Allergen.all.select {|allergen| allergen.user == self}.map {|allergen| allergen.ingredient}
   end
   def top_three_recipes
     recipes.sort {|first, second| first.rating <=> second.rating }[-3..-1]
   end
   def most_recent_recipe
     recipes.last
+  end
+  def safe_recipes
+    recipes.select do |recipe|
+      # binding.pry
+      recipe.ingredients.select do |ingredient|
+        # binding.pry
+        allergens.select do |allergen|
+          # binding.pry
+          ingredient != allergen
+        end
+      end
+    end
   end
 end
